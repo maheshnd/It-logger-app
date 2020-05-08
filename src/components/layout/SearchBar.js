8edb7ce9aee984faa52293290e76efe3a7ef2c20
maheshnd <deshmukh.mahesh.n@gmail.com>
2020-05-08 21:49:25 +0530

    removed some error

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { searchLogs } from '../../action/logAction';
 import PropTypes from 'prop-types';
